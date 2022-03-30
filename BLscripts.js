0e04c0528daa237d26a76937c7e451e20f42c3aa
Heshan <heshanpun@gmail.com>
2022-03-30 15:04:38 -0600

    Created html skeleton and some CSS

---
+++ BLscripts.js
@@ -0,0 +1,46 @@
+//global vars and xml object
+var xmlObject = new XMLHttpRequest();
+var platnerSelected;
+
+//show form when user selects a type of planter
+
+// show form - Square/Rectangular Cubes
+function showSquareForm(){
+
+}
+
+
+// show form - Flat bottomed cylinders
+function showFlatBottmForm(){
+    
+}
+
+
+// show form - ½ Spherical
+function showSphericalForm(){
+    
+}
+
+
+// show form - Truncated cone
+function showTruncForm(){
+    
+}
+
+
+// print message on page
+function printToPage(){
+
+}
+
+//returns customerInfo
+function customerInfo(){
+
+}
+
+
+
+
+
+
+
